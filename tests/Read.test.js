ecb15fe4c5843b83be70fef7105c50e397d7bae8
anisshili <anisshili.as@gmail.com>
2021-02-28 20:17:30 -0500

    finished tests for Read class

--- tests/Read.test.js
+++ tests/Read.test.js
@@ -1,11 +1,14 @@
 const Read = require('../CSV/Read');
+const City = require('../Models/City')
+const file = 'data/worldcities.csv'
 
 test('test the instantiation of the Read class', () => {
-    let read = new Read();
+    let read = new Read(file, City);
+
     expect(read).toBeInstanceOf(Read);
 });
 
-// test('test getRecords method', () => {
-//     let result = Read.getRecords();
-//     expect(result).toBe(false);
-// });
+test('test getRecords method', () => {
+    const result = Read.getRecords(file,City);
+    expect(result).toHaveLength(6)
+});
